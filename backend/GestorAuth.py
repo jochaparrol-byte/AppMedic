65969aaa9c5af02d1cd701a042394c62a7ee5f65
Darwin <johnlandaeta1000@gmail.com>
2026-03-18 19:03:58 -0500

    feat(GestorAuth): Se agrega el metodos de inicio de sesion y registro.*Es necesario correccion en el registro, segun el diccionario*

--- backend/GestorAuth.py
+++ backend/GestorAuth.py
@@ -1,6 +1,8 @@
 from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Text
 from sqlalchemy.orm import relationship, declarative_base
+from models import Usuario, Medico, Paciente, Citas
 import bcrypt
+
 #print(bcrypt.__version__)
 
 Base=declarative_base()
@@ -11,4 +13,16 @@ class GestorAuth:
     def __init__(self, gestor_bd):
         self.db=gestor_bd
     
-    
+    def registrar_usuario(self, nombre, especialidad, password):#pensar en como agregar el resto de informacion
+        bytes_password=password.encode('utf-8')
+        
+    def iniciar_sesion(self, correo, contrasena_intento):
+
+        usuario=self.db.obtener_por_columna(Usuario, "email", correo)
+        if not usuario:
+            return None, "Usuario no fue encontrado"
+        
+        if usuario.verificar_password(contrasena_intento):
+            return usuario, "Logeo exitoso"
+        return None, "Credenciales incorrectas"
+
--- backend/GestorBaseDatos.py
+++ backend/GestorBaseDatos.py
@@ -13,20 +13,27 @@ class GestorBaseDatos:
 
     
     def __init__(self):
-        self.usuario=os.getenv("DB_USUARIO")
-        self.password=os.getenv("DB_PASSWORD")
-        self.host=os.getenv("DB_HOST")
-        # Guardamos la URL de conexión (fácil de cambiar a AWS luego)
-        self.port=os.getenv("DB_PORT")
-        self.database_name=os.getenv("DB_NAME")
-        self.url = f"postgresql+psycopg2://{self.usuario}:{self.password}@{self.host}:{self.port}/{self.database_name}?sslmode=require"
+
+        entorno=os.getenv("ENTORNO", "local")
+        if(entorno=="produccion"): #produccion para que la conexion con la base de datos sea con la base de datos en AWS
+            self.usuario=os.getenv("DB_USUARIO")
+            self.password=os.getenv("DB_PASSWORD")
+            self.host=os.getenv("DB_HOST")
+            
+            self.port=os.getenv("DB_PORT")
+            self.database_name=os.getenv("DB_NAME")
+            self.url = f"postgresql+psycopg2://{self.usuario}:{self.password}@{self.host}:{self.port}/{self.database_name}?sslmode=require"
+        else: #cuando diga local la conexion se realizara con la bd de prueba, la local
+            print("🏠 Iniciando en modo LOCAL (SQLite)...")
+            
+            self.url = "sqlite:///base_datos_pruebas.db"
         
-        self.engine = create_engine(self.url)
+        self.engine = create_engine(self.url) #el engine sera por asi decirlo el mayordomo y traductor a la vez. Estamos dando ordenes con python a una base de datos que generalmente esta en "lenguaje" sql, por lo cual necesita traduccion. Asi como tambien sirve como sala donde esperaran su turno para que el mayordomo lleve las solicitudes que haremos
         
         Base.metadata.create_all(self.engine)
 
         ClaseSesion=sessionmaker(bind=self.engine)
-        self.sesion=ClaseSesion()
+        self.sesion=ClaseSesion() #sesion somos nosotros, le estamos diciendo al mayordomo: "oye, necesito que hagas algo por mi"
 
         print("✅ Conexión a AWS exitosa.")
 
@@ -42,4 +49,4 @@ class GestorBaseDatos:
     
     def cerrar_sesion(self):
         self.sesion.close()
-        print("Sesion cerrada")
+        print("Sesion cerrada") 
